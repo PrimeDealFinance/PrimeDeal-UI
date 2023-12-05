4de1de426ba4545909b4b038c2c3373fcea5f1f6
Oleg Trenkin <o.trenkin@ya.ru>
2023-12-05 19:56:05 +0300

    added deploy ci step

--- packages/contract/script/DeployScript.s.sol
+++ packages/contract/script/DeployScript.s.sol
@@ -17,15 +17,3 @@ contract DeployScript is Script {
         vm.stopBroadcast();
     }
 }
-/*
-ДЕПЛОЙ С ВЕРИФИКАЦИЕЙ ЭТИМ СКРИПТОМ. ПРОПИСАТЬ ТУТ СВОЙ ПРИВАТНИК. ОБРАТИТЬ ВНИМАНИЕ НА RPC
-forge create 
---rpc-url 'https://lb.drpc.org/ogrpc?network=polygon-mumbai&dkey=AmvHqebJ6k7ThQWeGwnLVmnsDTC3hx0R7qTGrkUU-y5L' 
---private-key '2b31898648bddb145bf821a1ЗАМЕНИТЬНАСВОЙ'  
---constructor-args 0xC36442b4a4522E871399CD717aBDD847Ab11FE88 0x1F98431c8aD98523631AE4a59f267346ea31F984 
---verify 
---legacy 
---etherscan-api-key 'R93KQB2UG493H148YUSDFQH9498RV9AJIJ'  
-src/PositionManager.sol:PositionManager
-                
-*/

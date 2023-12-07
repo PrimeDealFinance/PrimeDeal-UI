4ff46bcb70c6e5772672b590b402c0cbc908c994
Oleg Trenkin <o.trenkin@ya.ru>
2023-12-08 00:30:28 +0300

    up

--- packages/contract/script/deploy.sh
+++ packages/contract/script/deploy.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#/bin/bash
 
 forge create \
     --rpc-url 'https://lb.drpc.org/ogrpc?network=polygon-mumbai&dkey=AmvHqebJ6k7ThQWeGwnLVmnsDTC3hx0R7qTGrkUU-y5L' \
@@ -9,4 +9,8 @@ forge create \
     --etherscan-api-key 'R93KQB2UG493H148YUSDFQH9498RV9AJIJ' \
     --json \
     --silent \
-    src/PositionManager.sol:PositionManager | tee deploy_info.txt
+    src/PositionManager.sol:PositionManager | tee script/out.txt
+
+echo "Extracting deployedTo address..."
+cat script/out.txt | head -n 1 | jq '.deployedTo' | tee script/contract_deploy_address.txt
+echo "Done"

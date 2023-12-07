ad7070783fd4bfefd336944a22f568950fc6cca0
Oleg Trenkin <o.trenkin@ya.ru>
2023-12-07 15:00:54 +0300

    fix

---
+++ packages/contract/script/deploy.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+
+forge create \
+    --rpc-url 'https://lb.drpc.org/ogrpc?network=polygon-mumbai&dkey=AmvHqebJ6k7ThQWeGwnLVmnsDTC3hx0R7qTGrkUU-y5L' \
+    --private-key '0xe713b29254fcbfa701a7210983075d4813eeedf6e5b22551e354a7b40e44c9c9' \
+    --constructor-args 0xC36442b4a4522E871399CD717aBDD847Ab11FE88 0x1F98431c8aD98523631AE4a59f267346ea31F984 \
+    --verify \
+    --legacy \
+    --etherscan-api-key 'R93KQB2UG493H148YUSDFQH9498RV9AJIJ' \
+    --json \
+    --silent \
+    src/PositionManager.sol:PositionManager | tee deploy_info.txt

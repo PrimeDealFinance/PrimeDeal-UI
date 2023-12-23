12f08b553ee5248032b562f9f7f3b521f24612bb
Oleg Trenkin <o.trenkin@ya.ru>
2023-12-24 00:37:16 +0300

    fs false

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.resolve.fallback = { fs: false };
     return config;
   },
 };

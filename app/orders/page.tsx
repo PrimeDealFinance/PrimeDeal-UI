b219d2c34d0f597128bfdcfc0f2f9ad7244c7e9c
Vladimir Solo <freeman.wushu@mail.ru>
2024-02-09 22:19:37 +0600

    create table version 2.0

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,14 +1,8 @@
 "use client";
 import Link from "next/link";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Avatar,
-} from '@mui/material';
+import Table from '@mui/joy/Table';
+import Avatar from '@mui/joy/Avatar';
+import Sheet from '@mui/joy/Sheet';
 import { COLUMNS } from "@/app/orders/COLUMNS";
 
 type User = {
@@ -22,70 +16,53 @@ type User = {
   link: string;
 };
 
-const DATA_FOR_TEST: User[] = [
-  {
-    id: 1,
-    avatar: "/sovaOrder.jpeg",
-    asset: "ETH",
-    type: "Buy",
-    feeBalance: "0.270000 ETH / 0.00 USDC",
-    orderBalance: "0.000000 ETH / 1.00 USDC",
-    usdBalance: "1.00",
-    link: "#",
-  },
-  {
-    id: 2,
-    avatar: "/sovaOrder.jpeg",
-    asset: "ETH",
-    type: "Sell",
-    feeBalance: "0.270000 ETH / 0.00 USDC",
-    orderBalance: "0.000000 ETH / 1.00 USDC",
-    usdBalance: "1.00",
-    link: "#",
-  },
-  {
-    id: 3,
-    avatar: "/sovaOrder.jpeg",
-    asset: "ETH",
-    type: "Sell",
-    feeBalance: "0.270000 ETH / 0.00 USDC",
-    orderBalance: "0.000000 ETH / 1.00 USDC",
-    usdBalance: "1.00",
-    link: "#",
-  },
-]
+const DATA_FOR_TEST: User[] = Array.from({ length: 6 }, (_, index) => ({
+  id: index + 1,
+  avatar: "/solidity.svg",
+  asset: "ETH",
+  type: index % 2 === 0 ? "Buy" : "Sell",
+  feeBalance: "0.270000 ETH / 0.00 USDC",
+  orderBalance: "0.000000 ETH / 1.00 USDC",
+  usdBalance: "5 690 USDC",
+  link: "#",
+}));
+
+const TEXT_ORDERS = {
+  title: 'My orders'
+}
 
 export default function Orders() {
   return (
-    <div className="flex flex-col h-screen flex flex-col items-center">
-      <div className="p-[20px] xl:w-[1262px] w-11/12 border-1-solid-#3D59AD rounded-[15px] bg-[#7980A580] mt-[110px]">
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
+    <div className="mt-[180px] h-screen flex flex-col items-center z-10 mb-20">
+      <div className="xl:w-[1200px] w-11/12">
+        <h1 className="self-start text-3xl font-bold">{TEXT_ORDERS.title}</h1>
+        <Sheet color="primary" className="p-5 mt-5 rounded-3xl">
+          <Table variant="plain">
+            <thead>
+              <tr>
                 {COLUMNS.map((column) => (
-                  <TableCell key={column.uid}>{column.name}</TableCell>
+                  <th key={column.uid}>{column.name}</th>
                 ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
+              </tr>
+            </thead>
+            <tbody>
               {DATA_FOR_TEST.map((row) => (
-                <TableRow key={row.id} >
-                  <TableCell>
+                <tr key={row.id} >
+                  <td>
                     <Link href={row.link} className="flex items-center">
                       <Avatar alt="Avatar" src={row.avatar} className="mr-3" />
                       <span>{row.asset}</span>
                     </Link>
-                  </TableCell>
-                  <TableCell>{row.type}</TableCell>
-                  <TableCell>{row.feeBalance}</TableCell>
-                  <TableCell>{row.orderBalance}</TableCell>
-                  <TableCell>{row.usdBalance}</TableCell>
-                </TableRow>
+                  </td>
+                  <td className={row.type === 'Sell' ? 'text-[#EF3131]' : 'text-[#6FEE8E]'}>{row.type}</td>
+                  <td>{row.feeBalance}</td>
+                  <td>{row.orderBalance}</td>
+                  <td>{row.usdBalance}</td>
+                </tr>
               ))}
-            </TableBody>
+            </tbody>
           </Table>
-        </TableContainer>
+        </Sheet>
       </div>
     </div>
   );

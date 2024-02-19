ff423227b31e4a21cc89e6c6f258ca26248428c6
0xduda <nkondrashinnn@gmail.com>
2024-02-19 19:24:06 +0700

    fix MainButton positioning, add responsive to orders table

--- app/orders/COLUMNS.ts
+++ app/orders/COLUMNS.ts
@@ -1,7 +1,7 @@
 export const COLUMNS = [
-  { name: "ASSET", uid: "asset" },
-  { name: "TYPE", uid: "type" },
-  { name: "FEE BALANCE", uid: "feeBalance" },
-  { name: "ORDER BALANCE", uid: "orderBalance" },
-  { name: "BEGINING, $", uid: "usdBalance" },
+  { name: "ASSET", uid: "asset", minWidth: 300 },
+  { name: "TYPE", uid: "type", minWidth: 300 },
+  { name: "FEE BALANCE", uid: "feeBalance", minWidth: 620 },
+  { name: "ORDER BALANCE", uid: "orderBalance", minWidth: 490 },
+  { name: "BEGINING, $", uid: "usdBalance", minWidth: 768},
 ];

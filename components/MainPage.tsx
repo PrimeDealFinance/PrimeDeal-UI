85f6abe13eb036c5727fc14d38f2060c042cd108
0xduda <nkondrashinnn@gmail.com>
2024-02-20 03:32:17 +0700

    add responsive to orders-info page and main buttons

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -1,4 +1,5 @@
 'use client'
+import React from "react";
 import { useWalletStore } from "@/service/store";
 import Image from "next/image";
 import MainButton from "@/components/MainButton";
@@ -7,6 +8,11 @@ import Link from "next/link";
 import { Background } from "@/components/Background";
 import { useRouter } from "next/navigation";
 import "@/app/font.css";
+import Drawer from '@mui/joy/Drawer';
+import MediaQuery from 'react-responsive';
+import { Button } from "@mui/joy";
+import ModalClose from "@mui/joy/ModalClose";
+import Burger from "@/public/burger.svg"
 
 export default function MainPage({
   children,
@@ -30,6 +36,21 @@ export default function MainPage({
       handleIsConnected();
     }
   };
+
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer =
+    (inOpen: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setOpen(inOpen);
+    };
   
   return (
     <main className="dark h-full flex flex-col relative text-foreground bg-[#010306] z-[0]">
@@ -43,16 +64,55 @@ export default function MainPage({
             className="w-auto absolute top-[45.07px] left-[70.34px] max-[767px]:top-[20.07px] max-[767px]:left-[50.34px]" />
         </Link>
       </div>
-      <div className="absolute top-[35px] z-[2] right-[70px] max-[1110px]:right-[-40px] max-[767px]:top-[10px]">
-        <MainButton text={isConnect ? miniText : "CONNECT WALLET"} handleConnectClick={handleConnectClick} />
-      </div>
-      <div className="absolute top-[90px] z-[2] right-[70px] max-[1110px]:right-[-40px] max-[767px]:top-[10px] max-[767px]:right-[130px] font-['GothamPro']">
-        {isConnect &&
-          <Link href={'/orders'}>
-            <MainButton text={"MY ORDERS"} />
-          </Link>
-        }
-      </div>
+      <MediaQuery minWidth={521}>
+        <div className="absolute top-[35px] z-[2] right-[70px] max-[1110px]:right-[-40px] max-[767px]:top-[10px]">
+          <MainButton text={isConnect ? miniText : "CONNECT WALLET"} handleConnectClick={handleConnectClick} />
+        </div>
+        <div className="absolute top-[90px] z-[2] right-[70px] max-[1110px]:right-[-40px] max-[767px]:top-[10px] max-[767px]:right-[130px] font-['GothamPro']">
+          {isConnect &&
+            <Link href={'/orders'}>
+              <MainButton text={"MY ORDERS"} />
+            </Link>
+          }
+        </div>
+      </MediaQuery>
+      <MediaQuery maxWidth={520}>
+        <div className="absolute top-[35px] z-[2] right-[70px] max-[1110px]:right-[10px] max-[767px]:top-[10px]">
+          <Button 
+            onClick={toggleDrawer(true)}
+            sx={{
+              width: '100px',
+              height: '35px'
+            }}
+            variant="plain"
+          >
+            <Image 
+              src={Burger}
+              alt=""
+              width={20}
+              height={20}
+            />
+          </Button>
+          <Drawer
+            open={open} 
+            onClose={toggleDrawer(false)}
+            anchor="right"
+            size="sm"
+          >
+            <ModalClose/>
+            <div className="mt-[20px]">
+              <MainButton text={isConnect ? miniText : "CONNECT WALLET"} handleConnectClick={handleConnectClick} />
+            </div>
+            <div className="mt-[60px] font-['GothamPro']">
+              {isConnect &&
+                <Link href={'/orders'}>
+                  <MainButton text={"MY ORDERS"} />
+                </Link>
+              }
+            </div>
+          </Drawer>
+        </div>
+      </MediaQuery>
       <Background />
       {children}
     </main>
